cc592fba016e0e171c71d79f746b0b990760b8bc
vinicius-dec <viniciusbellonidesouza@gmail.com>
2024-08-28 19:28:34 -0300

    Add files via upload

--- exercicios/atividade1.js
+++ exercicios/atividade1.js
@@ -4,7 +4,7 @@ let lado1= 3
 let lado2= 10
 let lado3=10
 
-    if(lado1===lado2===lado3 && lado1===lado3){
+    if(lado1===lado2 && lado2===lado3 && lado1===lado3){
         console.log("Triangulo equilatero")
     }
 
