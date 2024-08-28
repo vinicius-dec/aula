08abeac3f9bdb9b819fbd5c98a29418d3b2b4cc3
vinicius-dec <viniciusbellonidesouza@gmail.com>
2024-08-28 19:21:47 -0300

    Add files via upload

--- exercicios/atividade2.js
+++ exercicios/atividade2.js
@@ -1,10 +1,16 @@
 //2-Faça um programa que verifique(usando if e selse) se uma letra digitada é "f" ou "M". conforme a letra escrever: F- Feminino, M-Masculino, Sexo inválido.
 
-
+const s1="F"
 const s2= "M"
+const r= "M"
 
-if(s1 && s2){
+if(r===s1){
     console.log(s1,"Feminino")
 }
-if(s2)
+else if(r===s2){
+    console.log(s2,"Masculino")
+}
+else{
+    console.log("Sexo invalido")
+}
   
