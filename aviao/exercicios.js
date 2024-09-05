a3af9583049850447f2210d1a3a8e0a06b93ada1
vinicius-dec <viniciusbellonidesouza@gmail.com>
2024-09-05 12:28:40 -0300

    Add files via upload

---
+++ aviao/exercicios.js
@@ -0,0 +1,39 @@
+
+
+
+
+let numero_de_passageiros_adultos= 3
+let numero_de_passageiros_criancas= 2
+let numero_de_adultos_na_executiva=2
+let numero_de_criancas_na
+const classe= "executivo"
+const destino= "Continente asiatico"
+const norte= "Norte do brasil"
+const sul= "Sul do brasil"
+const asiatico= "Continente asiatico"
+const europeu= "Continente europeu"
+const africano= "Continente africano"
+
+let norte_economica_adulto= 2500
+let norte_economica_crianca= 1800
+
+let norte_executiva_adulto= norte_economica_adulto + (norte_economica_adulto*20 )/100
+let norte_executiva_crianca=norte_economica_crianca + (norte_economica_crianca*10 )/100
+
+let sul_economica= 3000
+let sul_executivo= 4000  
+
+let asiatico_economica= 4600
+let asiatico_executiva= 6000
+
+let europeu_economica= 5800
+let europeu_executivo= 7300
+
+let africano_economica= 4200
+let africano_executivo= 6900
+
+
+if (destino=norte){
+    if (classe="executivo")
+    else if(classe="economico")
+}
